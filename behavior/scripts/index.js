0cbbe752388eca89d46dc570628364abbfe2f874
alexandrutata <alexandru_tata@yahoo.co.uk>
2016-12-08 21:19:39 +0200

    index commit

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
-exports.handle = (client) => {
-  // Create steps
+exports.handle = function handle(client) {
   const sayHello = client.createStep({
     satisfied() {
       return Boolean(client.getConversationState().helloSent)
@@ -13,11 +12,9 @@ exports.handle = (client) => {
         documentation_link: 'http://docs.init.ai',
       })
       client.addResponse('provide/instructions')
-
       client.updateConversationState({
         helloSent: true
       })
-
       client.done()
     }
   })
@@ -29,21 +26,75 @@ exports.handle = (client) => {
 
     prompt() {
       client.addResponse('apology/untrained')
+     client.done()
+    }
+  })
+
+  const handleGreeting = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addTextResponse('Hello world, I mean human')
       client.done()
     }
   })
 
+  const handleGoodbye = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addTextResponse('See you later!')
+      client.done()
+    }
+  })
+
+  const collectCity = client.createStep({
+	  satisfied() {
+		return Boolean(client.getConversationState().weatherCity)
+	  },
+
+	  prompt() {
+		// Need to prompt user for city    
+		console.log('Need to ask user for city')
+		client.done()
+	  },
+	})
+
+  const provideWeather = client.createStep({
+	  satisfied() {
+		return false
+	  },
+
+	  prompt() {
+		// Need to provide weather
+		client.done()
+	  },
+	})
+  
+  client.runFlow({
+	  classifications: {},
+	  streams: {
+		main: 'getWeather',
+		hi: [sayHello],
+		getWeather: [collectCity, provideWeather],
+	  }
+	})
+  
   client.runFlow({
     classifications: {
-      // map inbound message classifications to names of streams
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
+      goodbye: 'goodbye',
+      greeting: 'greeting'
     },
     streams: {
+      goodbye: handleGoodbye,
+      greeting: handleGreeting,
       main: 'onboarding',
       onboarding: [sayHello],
-      end: [untrained],
-    },
+      end: [untrained]
+    }
   })
 }
